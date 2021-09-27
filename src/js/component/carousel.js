cc721bc55053e669dd993931fb761c2c01dc15e6
edianibarrola <edianibarrola@gmail.com>
2021-09-27 20:58:36 +0000

    update styles

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -5,27 +5,25 @@ export const MyCarousel = () => {
 	return (
 		<Carousel>
 			<Carousel.Item>
-				<img className="d-block w-100" src="holder.js/800x400?text=First slide&bg=373940" alt="First slide" />
-				<Carousel.Caption>
-					<h3>First slide label</h3>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-				</Carousel.Caption>
+				<img
+					className="d-block w-100 rounded"
+					src="https://res.cloudinary.com/petrep/image/upload/v1632756459/portfolio/codepic_1_r40pbk.jpg"
+					alt="First slide"
+				/>
 			</Carousel.Item>
 			<Carousel.Item>
-				<img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
-
-				<Carousel.Caption>
-					<h3>Second slide label</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				</Carousel.Caption>
+				<img
+					className="d-block w-100 rounded"
+					src="https://res.cloudinary.com/petrep/image/upload/v1632756459/portfolio/musicpic_gqxffq.jpg"
+					alt="Second slide"
+				/>
 			</Carousel.Item>
 			<Carousel.Item>
-				<img className="d-block w-100" src="holder.js/800x400?text=Third slide&bg=20232a" alt="Third slide" />
-
-				<Carousel.Caption>
-					<h3>Third slide label</h3>
-					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-				</Carousel.Caption>
+				<img
+					className="d-block w-100 rounded"
+					src="https://res.cloudinary.com/petrep/image/upload/v1632756459/portfolio/filmpic_ya5ync.jpg"
+					alt="Third slide"
+				/>
 			</Carousel.Item>
 		</Carousel>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,18 @@ import "../../styles/home.scss";
 import { MyCarousel } from "../component/carousel";
 
 export const Home = () => (
-	<div className="container text-center mt-5" style={{ height: "90vh" }}>
-		<div className="row">
-			<div className="col">
-				<h1>Edian Ibarrola</h1>
-				<h2>Full-Stack Developer</h2>
-				<p>I make stuff.</p>
+	<div className="container text-center mt-5 " style={{ border: "2px solid black" }}>
+		<div className="row " style={{ height: "75vh" }}>
+			<div className="col-12 col-md-6 d-flex flex-column justify-content-center p-3 ">
+				<div className="col-12 ">
+					<h1>Edian Ibarrola</h1>
+					<h2>Full-Stack Developer</h2>
+					<p>I make stuff.</p>
+				</div>
+
+				<div className="col-12">
+					<MyCarousel />
+				</div>
 				<div className="mx-auto col-12 d-flex align-items-center justify-content-around">
 					<a href="http://www.edianibarrola.com">
 						<i className="fas fa-home" />
@@ -26,20 +32,16 @@ export const Home = () => (
 						<i className="fab fa-instagram" />
 					</a>
 				</div>
-				<div className="col-12">
-					<MyCarousel />
-				</div>
 			</div>
-			<div className="col">
+
+			<div className="col-12 col-md-6 d-flex justify-content-center align-items-center  ">
 				<img
-					style={{ height: "50vh" }}
+					className="img-responsive rounded"
+					style={{ "max-height": "45vh", border: "2px solid #4e3658" }}
 					src="https://res.cloudinary.com/petrep/image/upload/v1632749359/portfolio/IMG_3730_kjewfq.jpg"
 					alt="Edian Ibarrola's headshot picture"
 				/>
 			</div>
 		</div>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
 	</div>
 );
