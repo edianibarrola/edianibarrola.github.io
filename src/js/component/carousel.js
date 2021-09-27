f7c48d4bf22744ce995bdaddecd4839e8391e50b
edianibarrola <edianibarrola@gmail.com>
2021-09-27 23:47:57 +0000

    fixed style

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -1,35 +1,36 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
 
 export const MyCarousel = () => {
 	return (
 		<Carousel variant="dark">
 			<Carousel.Item interval={15000}>
-				<a href="/code">
+				<Link to="/code">
 					<img
 						className="d-block w-100 rounded"
 						src="https://res.cloudinary.com/petrep/image/upload/v1632756459/portfolio/codepic_1_r40pbk.jpg"
 						alt="First slide"
 					/>
-				</a>
+				</Link>
 			</Carousel.Item>
 			<Carousel.Item interval={3000}>
-				<a href="/music">
+				<Link to="/music">
 					<img
 						className="d-block w-100 rounded"
 						src="https://res.cloudinary.com/petrep/image/upload/v1632756459/portfolio/musicpic_gqxffq.jpg"
 						alt="Second slide"
 					/>
-				</a>
+				</Link>
 			</Carousel.Item>
 			<Carousel.Item interval={3000}>
-				<a href="/film">
+				<Link to="/film">
 					<img
 						className="d-block w-100 rounded"
 						src="https://res.cloudinary.com/petrep/image/upload/v1632756459/portfolio/filmpic_ya5ync.jpg"
 						alt="Third slide"
 					/>
-				</a>
+				</Link>
 			</Carousel.Item>
 		</Carousel>
 	);
